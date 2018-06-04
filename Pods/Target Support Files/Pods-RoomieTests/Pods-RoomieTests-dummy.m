6865a0b02f61bd56631721c78d6d62753dbfbf3c
Zachary Richardson <zacharyrichardson@Zacharys-Mac-mini.local>
2018-06-03 17:02:37 -0700

    Added DataTester

--- Pods/Target Support Files/Pods-RoomieTests/Pods-RoomieTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RoomieTests : NSObject
-@end
-@implementation PodsDummy_Pods_RoomieTests
-@end
---
+++ RoomieDataTester/RoomieDataTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoomieDataTester
+//
+//  Created by Zachary Richardson on 6/3/18.
+//  Copyright © 2018 Zachary Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
